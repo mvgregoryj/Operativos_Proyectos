173313e8e4c103983fcc91ff5b94a3bdbb8e6933
Jonathan <ELKOMI2303>
2021-06-07 23:34:16 -0400

    Parte de Procesos lista falta acomodar para Proceso 1 y 2

--- Proyecto_1/Proyecto.c
+++ Proyecto_1/Proyecto.c
@@ -6,15 +6,20 @@
 #include <string.h>
 #include <unistd.h>
 #include<sys/wait.h>
-
+#include <pthread.h>
 
 int Tellme_lines(char *Archivo);
 int esPrimo(int numero);
-void Separacion_lineas(Nodo **a[],int Num_process,int M,Nodo **Lista_Numeros, Nodo **P1,Nodo **P2,Nodo **P3,Nodo **P4,Nodo **P5,Nodo **P6,Nodo **P7,Nodo **P8,Nodo **P9);
-char DeEnteroaCaracter(int numero);
+void Separacion_lineas(Nodo **a[],int Num_process,int M,Nodo **Lista_Numeros,Nodo **P0, Nodo **P1,Nodo **P2,Nodo **P3,Nodo **P4,Nodo **P5,Nodo **P6,Nodo **P7,Nodo **P8,Nodo **P9, Nodo **Lista_Tareas);
 void Comprobar_Numero_Primo(Nodo **a[], int Numero_de_proceso, int Numero_Tareas);
 void inicializarProcesos(Nodo **Lista_Tareas, int Proceso,int Tareas);
-void AsignarProceso(Nodo **Lista_Tareas, int Proceso,int Tareas);
+void ProgramError(long x );
+struct Parametros{
+	int Numero_Hilos;
+	Nodo *Lista_Numeros;
+};
+
+
 int main(int argc, char *argv[])
 {
 	if(strcmp(argv[2],"-p")==0||strcmp(argv[2],"-P")==0){
@@ -24,13 +29,15 @@ int main(int argc, char *argv[])
 		int Numero=0;
 		int i=0;
 		long Num_process;
-		Nodo **a[9];  // arreglo de apuntadores donde iran el apuntador a conjuntos de lineas las culaes se les asignaran a los Procesos hijos por el padre.
+		Nodo **a[10];  // arreglo de apuntadores donde iran el apuntador a conjuntos de lineas las culaes se les asignaran a los Procesos hijos por el padre.
 		Nodo *Lista_Numeros=NULL;  //Inicializa la lista de Numeros que seran comprobados.
-		Nodo *P1,*P2,*P3,*P4,*P5,*P6,*P7,*P8,*P9=NULL;
+		Nodo *P0,*P1,*P2,*P3,*P4,*P5,*P6,*P7;
+		Nodo *P8=NULL;
+		Nodo *P9=NULL;
 		
 		Nodo *Lista_Tareas=NULL;  // Inicializa Una lista donde estaran en cada Nodo la cantidad de tareas que realizara cada Proceso/Hilo.
 		Num_process=strtol(argv[3],NULL,10);  //Numero de procesos solicitados por consola 
-
+		ProgramError(Num_process);
 		FILE *file=fopen(argv[1], "rt");
 		if (file==NULL){
 			perror("Error en la apertura del archivo");
@@ -49,20 +56,14 @@ int main(int argc, char *argv[])
 
 		}
 		fclose(file);
-		Separacion_lineas(a,Num_process,count,&Lista_Numeros,&P1,&P2,&P3,&P4,&P5,&P6,&P7,&P8,&P9);
+		Separacion_lineas(a,Num_process,count,&Lista_Numeros,&P0,&P1,&P2,&P3,&P4,&P5,&P6,&P7,&P8,&P9,&Lista_Tareas);
 		int wpid;
 		pid_t	pid;
-		int fd[2];
-		pipe(fd);
-		char buf[10];
-		int N=Num_process-1;
-		int Tareas=count/N;
-		inicializarProcesos(&Lista_Tareas,0,Tareas);
+		int N=Num_process;
 
 		for(i=0;i<N;i++){
 			pid=fork();    //Se hace fork()
 			if(pid==0){		// si el proceso se crea bien, terminamos el ciclo for
-				sleep(1);
 				break;
 
 			}else if(pid==-1){
@@ -70,27 +71,24 @@ int main(int argc, char *argv[])
 				exit(1);
 				break;
 			}else{
-				if(i!=0 && i!=N-1){
-					AsignarProceso(&Lista_Tareas,i,Tareas);
-				}
-				else if(i==N-1){
-					Tareas=(count/N)+(count%N);
-					printf("%d\n",Tareas );
-					AsignarProceso(&Lista_Tareas,i,Tareas);
-				}
-				sleep(2);
+				EliminarInicio(&Lista_Tareas);
+				EliminarInicio(&Lista_Tareas);
+				sleep(1);
 
 			}
 		}
 		if(pid==0){
-			ImprimirLista(Lista_Tareas);
-				printf("\n");
+			int x=EliminarInicio(&Lista_Tareas);
+			int y=EliminarInicio(&Lista_Tareas);
+			Comprobar_Numero_Primo(a,x,y);
+		
+
 			//printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid());
       exit(0);
 
 		}
 		else{
-			for(int i=0;i<Num_process;i++){
+			for(int i=0;i<N;i++){
 				if((wpid=wait(NULL))>=0){
 					 //printf("Proceso: %d  terminado\n",wpid);
        
@@ -130,31 +128,37 @@ int main(int argc, char *argv[])
 		int count=Tellme_lines(argv[1]);
 		int Numero=0;
 		int i=0;
-		long Num_process;
-		Num_process=strtol(argv[3],NULL,10);  //Numero de procesos solicitados por consola 
-		
-
+		long Num_threads;
+		Nodo *Lista_Numeros;
+		Num_threads=strtol(argv[3],NULL,10);  //Numero de procesos solicitados por consola 
+		ProgramError(Num_threads);
 		FILE *file=fopen(argv[1], "rt");
 		if (file==NULL){
 			perror("Error en la apertura del archivo");
 		return 1;
 		}
 
-		int Lines[count];
 		while(!feof(file)&&i!=count){
-		
+			if(i==0){
+				fscanf(file,"%d",&Numero); // extrae del archivo el numero de la linea.
+				InsertarInicio(&Lista_Numeros, Numero); // inserta en la lista enlazada el numero respectivo de cada linea
+				i=i+1;  //El número de lineas
+			}
 			fscanf(file,"%d",&Numero);
-			Lines[i]=Numero;
+			InsertarFinal(&Lista_Numeros, Numero);
 			i=i+1;  //El número de lineas
 
 		}
-		fclose(file);
-
 
 	}
 
 
 }
+
+
+
+
+
 int esPrimo(int numero) {
   if (numero == 0 || numero == 1){
 
@@ -204,21 +208,33 @@ int Tellme_lines(char *Archivo)
 		return count;
 }
 
-void Separacion_lineas(Nodo **a[],int Num_process,int M,Nodo **Lista_Numeros, Nodo **P1,Nodo **P2,Nodo **P3,Nodo **P4,Nodo **P5,Nodo **P6,Nodo **P7,Nodo **P8,Nodo **P9){
-	a[0]=P1,a[1]=P2,a[2]=P3,a[3]=P4,a[4]=P5,a[5]=P6,a[6]=P7,a[7]=P8,a[8]=P9;
-	int N=Num_process-1;
+void Separacion_lineas(Nodo **a[10],int Num_process,int M,Nodo **Lista_Numeros,Nodo **P0, Nodo **P1,Nodo **P2,Nodo **P3,Nodo **P4,Nodo **P5,Nodo **P6,Nodo **P7,Nodo **P8,Nodo **P9,Nodo **Lista_Tareas){
+	a[0]=P0,a[1]=P1,a[2]=P2,a[3]=P3,a[4]=P4,a[5]=P5,a[6]=P6,a[7]=P7;
+	a[8]=P8;
+	a[9]=P9;
+	int N=Num_process;
 	int Tarea1= M/N;
 	int Tarea2=M/N+(M%N);
 	int x;
 
 	for(int i=0;i<N;i++){
-		if(i!=N-1){
+		if(i!=(N-1)){
+			if (i==0){
+				InsertarInicio(Lista_Tareas,i);
+				InsertarFinal(Lista_Tareas,Tarea1);
+			}else{
+				InsertarFinal(Lista_Tareas,i);
+				InsertarFinal(Lista_Tareas,Tarea1);
+			}
+							
 			for(int j=0;j<Tarea1;j++){
 				x=EliminarInicio(Lista_Numeros);
 				InsertarInicio(a[i],x);
 			}
 		}
-		else{
+		else if(i==(N-1)){
+			InsertarFinal(Lista_Tareas,i);
+			InsertarFinal(Lista_Tareas,Tarea2);
 			for(int j=0;j<Tarea2;j++){
 				x=EliminarInicio(Lista_Numeros);
 				InsertarInicio(a[i],x);
@@ -227,55 +243,46 @@ void Separacion_lineas(Nodo **a[],int Num_process,int M,Nodo **Lista_Numeros, No
 	}
 
 }
-char DeEnteroaCaracter(int numero){
- 	return numero +'0';
- 	
-}
 
-void Comprobar_Numero_Primo(Nodo **a[], int Numero_de_proceso, int Numero_Tareas)
+
+void Comprobar_Numero_Primo(Nodo **a[], int Numero_de_proceso, int Numero_de_Tareas)
 {	
-	int N=Numero_de_proceso-1;
 	char Texto_completo[50];
 	char texto2[]=".txt";
 	char texto1[50];
 	int Chequear_Numero;
 	char Numero_chequeado[50];
-
-	for(int i=0 ; i<N;i++){
-		sprintf(texto1, "%d", i);
-		strcat(texto1,texto2);;
-		FILE *file=fopen(texto1,"w");
-		if(file==NULL){
-			perror("Error en la creacion del archivo \n\n");
+	int i;
+	i=Numero_de_proceso;
+	sprintf(texto1, "%d", i);
+	strcat(texto1,texto2);;
+	FILE *file=fopen(texto1,"w");
+	if(file==NULL){
+		perror("Error en la creacion del archivo \n\n");
 
 		}else{
 
-			for(int j=0;j<Numero_Tareas;j++){
-				Chequear_Numero= EliminarInicio(a[i]);
-				if(esPrimo(Chequear_Numero)==1){
-					fprintf(file, "%d %c", Chequear_Numero,'1');
-				}else{
-					fprintf(file, "%d %c",Chequear_Numero,'0');
-				}
-			
-			 	fputc('\n',file) ;
+		for(int j=0;j<Numero_de_Tareas;j++){
+			Chequear_Numero= EliminarInicio(a[i]);
+			if(esPrimo(Chequear_Numero)==1){
+				fprintf(file, "%d %c", Chequear_Numero,'1');
+			}else{
+				fprintf(file, "%d %c",Chequear_Numero,'0');
+			}
+			fputc('\n',file) ;
 			}
 			fflush(file);
 			fclose(file);
 		}
 
 
+
+}
+void ProgramError(long x ){
+	if(x>=11|| x<=0){
+		printf("Entrada No valida, Asegurece de que el numero de procesos Este entre 1 y 10");
+		exit(-1);
 	}
+	
 }
 
-void inicializarProcesos(Nodo **Lista_Tareas, int Proceso,int Tareas){
-	InsertarInicio(Lista_Tareas,Tareas);
-	InsertarFinal(Lista_Tareas,Proceso);
-}
-void AsignarProceso(Nodo **Lista_Tareas, int Proceso,int Tareas){
-	EliminarInicio(Lista_Tareas);
-	EliminarInicio(Lista_Tareas);
-	InsertarInicio(Lista_Tareas,Tareas);
-	InsertarFinal(Lista_Tareas,Proceso);
-
-}

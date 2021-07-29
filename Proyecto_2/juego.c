df3d0b5146832fb75f8e83471662ead40c912e72
Daniela <randrade.daniela@gmail.com>
2021-07-28 23:25:20 -0400

    Con funcion de lector y escritor

--- Proyecto_2/juego.c
+++ Proyecto_2/juego.c
@@ -11,11 +11,16 @@
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
 int Tellme_lines(char *Archivo);
-void ImprimirMundo(int **Mundo, int filas, int columnas);
-int ** Generar_Mundo(char * Archivo, int filas, int columnas);
 int ** LeerArchivoProceso(int filas, int columnas, int numero_de_procesos, int Proceso, int cantidad_de_lineas, char *Archivo);
 void Imprimir(int ** ma, int nfilas, int ncol);
 void juego(int Num_process, int filas, int columnas, char *archivo);
+void lector(int Proceso, int Num_process);
+void escritor(int Proceso, int Num_process);
+
+int **pipes_primario;
+int **pipes_secundario;
+long Num_process;
+int **Mundito;
 
 
 int main(int argc, char *argv[] ){
@@ -30,8 +35,6 @@ int main(int argc, char *argv[] ){
 	Num_generaciones=strtol(argv[2],NULL,10);
 	Num_visualizaciones=strtol(argv[3],NULL,10);
 	Numero_lineas=Tellme_lines(argv[4]);
-
-	printf("%d\n",Numero_lineas);
 
 	FILE *file=fopen(argv[4],"rt");
 	if (file==NULL){
@@ -52,20 +55,19 @@ int main(int argc, char *argv[] ){
 	fclose(file);
 
 	juego(Num_process,filas,columnas,argv[4]);
-	
-
 }
 
 
 void juego(int Num_process, int filas, int columnas, char *archivo){
-
-	// Esto es lo que he hecho
 	
-	int pipes_primario[Num_process-1][2];
-	int pipes_secundario[Num_process-1][2];
+	//pipes[x][0] lee
+	//pipes[x][1] escribe
+
+	int pipes_primario[Num_process-1][2];   // Escribira en el proceso i y leera del proceso i-1
+	int pipes_secundario[Num_process-1][2]; // Escribira en el proceso i y leera del proceso i+1
 	int pids[Num_process];
 	int **Mundito[Num_process];
-	int nlineas = (filas/Num_process);
+	int nlineas = (filas/Num_process); //Lineas que le tocaria a cada proceso
 
 	for(int i=0; i<Num_process; i++){
 		if(pipe(pipes_primario[i]) == -1){
@@ -77,21 +79,20 @@ void juego(int Num_process, int filas, int columnas, char *archivo){
 	}
 
 	for (int i=0; i<Num_process; i++){
-		
 		pids[i] = fork();
-
 		if(pids[i] == -1){
 			printf("Error creando el proceso.");
 		}
 
 		if (pids[i] == 0){
 			//Proceso hijo
-			printf("proceso: %d\n", i);
+			printf("proceso hijo: %d\n", i);
 			Mundito[i] = LeerArchivoProceso(filas,columnas,Num_process,i,nlineas,archivo);
-			Imprimir(Mundito[i],filas,columnas);
+			Imprimir(Mundito[i],nlineas,columnas);
 						
-			
 			for(int j=0; j<Num_process;j++){
+
+				//Se cierran los pipes que no usara el proceso i
 
 				if(i != j){
 					close(pipes_primario[j][0]);
@@ -115,54 +116,14 @@ void juego(int Num_process, int filas, int columnas, char *archivo){
 				close(pipes_primario[0][0]);
 			}
 
-			/**
-			int x;
-			if(i<Num_process-1){
-				if(read(pipes_primario[i][0],&x,sizeof(int)) == -1){
-					printf("Error leyendo\n");
-				}
-			}
-
-			if(i>0){
-				if(read(pipes_secundario[i][0],&x,sizeof(int)) == -1){
-					printf("Error leyendo\n");
-				}
-			}
-
-			//printf("Se obtiene %d\n", x);
-
-			//x++;
-
-			if(i<Num_process-1){
-				if(write(pipes_secundario[i+1][1],&x,sizeof(int)) == -1){
-					printf("Error escribiendo\n");
-				}
-			}
-
-			if(i>0){
-				if(write(pipes_primario[i-1][1],&x,sizeof(int)) == -1){
-					printf("Error escribiendo\n");
-				}
-			}
-			//printf("Se envia %d\n", x);*/
-
 			return;
 		}
+		wait(NULL); // Espera que cada hijo termine para que venga el siguiente (Esto para ver si funcionan las otras funciones pero se debe quitar porque no tiene sentido crear uno tras otro)
+	
 		
 	}
-/*
-	int y = 5;
-	//printf("Main process sent %d\n", y);
-	if(write(pipes_secundario[0][1], &y, sizeof(int))==-1){
-		printf("Error al escribir\n");
-	}
-
-	if(read(pipes_secundario[Num_process][0], &y, sizeof(int))==-1){
-		printf("Error al escribir\n");
-	}
-
-	printf("Resultado final %d\n", y);*/
 	
+	// El padre espera a los hijos
 	for (int i=0; i<Num_process; i++){
 		wait(NULL);
 	}
@@ -170,12 +131,53 @@ void juego(int Num_process, int filas, int columnas, char *archivo){
 
 }
 
+/** Para leer pipes */
+void lector(int Proceso, int Num_process){
+
+	int i = Proceso;
+	
+	if(i<Num_process-1){
+		if(read(pipes_primario[i][0],Mundito[i],sizeof(int)) == -1){
+			printf("Error leyendo el pipe\n");
+		}
+	}
+
+	if(i>0){
+		if(read(pipes_secundario[i][0],Mundito[i],sizeof(int)) == -1){
+			printf("Error leyendo el pipe\n");
+		}
+	}
+
+}
+
+/** Para escribir en los pipes */
+void escritor(int Proceso, int Num_process){
+
+	int i = Proceso;
+
+	if(i<Num_process-1){
+		if(write(pipes_secundario[i+1][1],Mundito[i+1],sizeof(int)) == -1){
+			printf("Error escribiendo en el pipe\n");
+		}
+	}
+
+	if(i>0){
+		if(write(pipes_primario[i-1][1],Mundito[i],sizeof(int)) == -1){
+			printf("Error escribiendo en el pipe\n");
+		}
+	}
+
+} 
+
+			
+
 /** Funcion para imprimir la matriz */
 void Imprimir(int ** ma, int nfilas, int ncol){
-  int m,n;
 
-  //Se imprime la matriz
-  for(m=0;m<nfilas;m++){
+  	int m,n;
+
+ 	//Se imprime la matriz
+  	for(m=0;m<nfilas;m++){		
         printf("\t\t\t\t");
         for(n=0;n<ncol;n++){
       
@@ -191,6 +193,7 @@ void Imprimir(int ** ma, int nfilas, int ncol){
         //Salto de linea al llegar al final de la fila
         printf("\n");
     }
+	
 }
 
 
@@ -213,6 +216,7 @@ int ** LeerArchivoProceso(int filas, int columnas, int numero_de_procesos, int P
 	int xx=0;
 	int ii=0;
 	bool lista_vacia=true;
+	
 	while(feof(flujo)==0){  // Se va iterando sobre las lineas del archivo hasta llegar a la primera linea del proceso en cuestion, luego cada linea se pasa a entero 
 		if(i==Empiezo_desde){   // y se va guardando en orden en la lista enlazada
 			while(xx<cantidad_de_lineas){
@@ -245,6 +249,7 @@ int ** LeerArchivoProceso(int filas, int columnas, int numero_de_procesos, int P
 		}
 	}
 
+	fclose(flujo);
 
 	parte_mundo = (int**)malloc(cantidad_de_lineas*sizeof(int*)); // se el doble apuntador que representa el arreglo con el tamaño de las columnas y las lineas que le toca
 	for(int i=0;i<cantidad_de_lineas;i++){                        // se va extrayendo de la lista numero por numero y se guarda en el arreglo, finalmente se obtiene la parte del mundo que le toca al proceso
